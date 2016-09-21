ecc764ebd66362d400d65f653030366d9984333a
ludelwic <luigi_delwiche@hotmail.com>
2016-09-21 12:14:24 +0200

    ,

--- GovernancePortal/wwwroot/js/app/governance.js
+++ GovernancePortal/wwwroot/js/app/governance.js
@@ -57,7 +57,7 @@ govApp.controller('GovernanceController', function GovernanceController($scope) 
                 specs: '500TB',
                 uri: 'https%3A%2F%2Fraw.githubusercontent.com%2Fludelwic%2Fdemogovernance%2Fmaster%2Fdemogovernance%2FTemplates%2FStorageAccount%2Fazuredeploystorage.json',
                 needsApproval: false
-            }
+            },
             {
             type: 'SQL Server AlwaysOn',
             size: 'Medium',
