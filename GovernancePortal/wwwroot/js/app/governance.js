9dbb61c47634ac6380950ae6059854553eb123aa
ludelwic <luigi_delwiche@hotmail.com>
2016-09-26 23:26:14 +0200

    pricing

--- GovernancePortal/wwwroot/js/app/governance.js
+++ GovernancePortal/wwwroot/js/app/governance.js
@@ -6,6 +6,7 @@ govApp.controller('GovernanceController', function GovernanceController($scope) 
                 type: 'Windows Server 2012 R2 DE',
                 size: 'Small',
                 specs: '1 CPU, 3.5 GB RAM',
+                internal price: '€200/month',
                 uri: 'https%3A%2F%2Fraw.githubusercontent.com%2Fludelwic%2Fdemogovernance%2Fmaster%2Fdemogovernance%2FTemplates%2FWindowsSmall%2Fazuredeploy.json',
                 needsApproval: false,
                 tags: ['Windows', 'Small']
@@ -14,6 +15,7 @@ govApp.controller('GovernanceController', function GovernanceController($scope) 
                 type: 'Windows Server 2012 R2 DE',
                 size: 'Medium',
                 specs: '2 CPU, 7.0 GB RAM',
+                internal price: '€400/month',
                 uri: 'https%3A%2F%2Fraw.githubusercontent.com%2Fludelwic%2Fdemogovernance%2Fmaster%2Fdemogovernance%2FTemplates%2FWindowsMedium%2Fazuredeploy.json',
                 needsApproval: false,
                 tags: ['Windows', 'Medium']
@@ -22,6 +24,7 @@ govApp.controller('GovernanceController', function GovernanceController($scope) 
                 type: 'Windows Server 2012 R2 DE',
                 size: 'Large',
                 specs: '8 CPU, 28 GB RAM',
+                internal price: '€800/month',
                 uri: 'https%3A%2F%2Fraw.githubusercontent.com%2Fludelwic%2Fdemogovernance%2Fmaster%2Fdemogovernance%2FTemplates%2FWindowsLarge%2Fazuredeploy.json',
                 needsApproval: true,
                 tags: ['Windows', 'Large']
@@ -30,6 +33,7 @@ govApp.controller('GovernanceController', function GovernanceController($scope) 
                 type: 'Linux Server Ubuntu',
                 size: 'Small',
                 specs: '1 CPU, 3.5 GB RAM',
+                internal price: '€125/month',
                 uri: 'https%3A%2F%2Fraw.githubusercontent.com%2Fludelwic%2Fdemogovernance%2Fmaster%2Fdemogovernance%2FTemplates%2FLinuxSmall%2FLinuxVirtualMachine.json',
                 needsApproval: false,
                 tags: ['Linux', 'Small', 'Ubuntu']
@@ -38,6 +42,7 @@ govApp.controller('GovernanceController', function GovernanceController($scope) 
                 type: 'Linux Server Ubuntu',
                 size: 'Medium',
                 specs: '2 CPU, 7.0 GB RAM',
+                internal price: '€250/month',
                 uri: 'https%3A%2F%2Fraw.githubusercontent.com%2Fludelwic%2Fdemogovernance%2Fmaster%2Fdemogovernance%2FTemplates%2FLinuxMedium%2FLinuxVirtualMachine.json',
                 needsApproval: false,
                 tags: ['Linux', 'Medium', 'Ubuntu']
@@ -46,6 +51,7 @@ govApp.controller('GovernanceController', function GovernanceController($scope) 
                 type: 'Linux Server Ubuntu',
                 size: 'Large',
                 specs: '8 CPU, 28 GB RAM',
+                internal price: '€500/month',
                 uri: 'https%3A%2F%2Fraw.githubusercontent.com%2Fludelwic%2Fdemogovernance%2Fmaster%2Fdemogovernance%2FTemplates%2FLinuxLarge%2FLinuxVirtualMachine.json',
                 needsApproval: true,
                 tags: ['Linux', 'Large', 'Ubuntu']
@@ -54,6 +60,7 @@ govApp.controller('GovernanceController', function GovernanceController($scope) 
                 type: 'SQL Database + Web App',
                 size: 'Small',
                 specs: '5GB',
+                internal price: '€100/month',
                 uri: 'https%3A%2F%2Fraw.githubusercontent.com%2Fludelwic%2Fdemogovernance%2Fmaster%2Fdemogovernance%2FTemplates%2FSQLDatabase%2Fazuredeploy.json',
                 needsApproval: false,
                 tags: ['SQL', 'Database', 'Web', 'app', 'Small']
@@ -62,6 +69,7 @@ govApp.controller('GovernanceController', function GovernanceController($scope) 
                 type: 'SQL Server AlwaysOn',
                 size: 'Medium',
                 specs: '5 VMs',
+                internal price: '€1250/month',
                 uri: 'https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2Fazure-quickstart-templates%2Fmaster%2Fsqlvm-alwayson-cluster%2Fazuredeploy.json',
                 needsApproval: true,
                 tags: ['SQL', 'Database', 'AlwaysOn', 'Medium']
@@ -70,6 +78,7 @@ govApp.controller('GovernanceController', function GovernanceController($scope) 
                 type: 'SAP Netweaver',
                 size: '3 tier',
                 specs: '30.000 SAPS',
+                internal price: '€1000/month',
                 uri: 'https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2Fazure-quickstart-templates%2Fmaster%2Fsap-3-tier-marketplace-image%2Fazuredeploy.json',
                 needsApproval: true,
                 tags: ['SAP', 'Netweaver', '3', 'tier']
@@ -78,6 +87,7 @@ govApp.controller('GovernanceController', function GovernanceController($scope) 
                 type: 'LAMP on Ubuntu',
                 size: 'A2 VM',
                 specs: 'Ubunty, MySQL, Apache and PHP',
+                internal price: '€175/month',
                 uri: 'https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2Fazure-quickstart-templates%2Fmaster%2Flamp-app%2Fazuredeploy.json',
                 needsApproval: false,
                 tags: ['LAMP', 'Linux', 'Ubuntu', 'MySQL', 'Apache', 'PHP']
@@ -86,6 +96,7 @@ govApp.controller('GovernanceController', function GovernanceController($scope) 
                 type: 'Storage',
                 size: 'LRS',
                 specs: '500TB',
+                internal price: '€25/month per TB',
                 uri: 'https%3A%2F%2Fraw.githubusercontent.com%2Fludelwic%2Fdemogovernance%2Fmaster%2Fdemogovernance%2FTemplates%2FStorageAccount%2Fazuredeploystorage.json',
                 needsApproval: false,
                 tags: ['Storage', 'LRS']
@@ -94,6 +105,7 @@ govApp.controller('GovernanceController', function GovernanceController($scope) 
                 type: 'Virtual Network',
                 size: 'Standard',
                 specs: '2 subnets',
+                internal price: '€25/month',
                 uri: 'https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2Fazure-quickstart-templates%2Fmaster%2F101-vnet-two-subnets%2Fazuredeploy.json',
                 needsApproval: false,
                 tags: ['Virtual', 'Network', 'Vnet', 'subnet', 'Standard']
@@ -102,6 +114,7 @@ govApp.controller('GovernanceController', function GovernanceController($scope) 
                 type: 'Ubuntu with Docker',
                 size: 'Standard F1',
                 specs: '1 CPU, 2GB',
+                internal price: '€100/month',
                 uri: 'https%3A%2F%2Fraw.githubusercontent.com%2Fludelwic%2Fdemogovernance%2Fmaster%2Fdemogovernance%2FTemplates%2FUbuntuDocker%2Fubuntudocker.json',
                 needsApproval: true,
                 tags: ['Ubnutu', 'Docker', 'Standard', 'Linux']
